78ba63c786db0044b1e4978a97b7e6f4e154a740
HERO <2729903242@qq.com>
2023-06-08 11:46:26 +0800

    更新

--- show_reorder_distribution/show_reorder_distribution.py
+++ show_reorder_distribution/show_reorder_distribution.py
@@ -39,15 +39,31 @@ def plot_edges(edges, threshold, title):
     for edge in edges:
         matrix[edge[0], edge[1]] = 1
 
-    # 绘制图像并添加标题
+    # 计算度数信息
+    degrees = {}
+    for edge in edges:
+        degrees[edge[0]] = degrees.get(edge[0], 0) + 1
+        degrees[edge[1]] = degrees.get(edge[1], 0) + 1
+    max_degree = max(degrees.values())
+    min_degree = min(degrees.values())
+    avg_degree = sum(degrees.values()) / len(degrees)
+
+    # 计算顶点数目和边数目
+    num_nodes = len(set(node for edge in edges for node in edge))
+    num_edges = len(edges)
+
+    # 绘制图像并添加标题和信息
     plt.imshow(matrix, cmap='Reds')
     plt.title(title)
+    text = f"Graph Information:\nNumber of Nodes: {num_nodes}\nNumber of Edges: {num_edges}\nMax Degree: {max_degree}\nMin Degree: {min_degree}\nAvg Degree: {avg_degree:.2f}"
+    plt.text(0.0, -0.15, text, transform=plt.gca().transAxes, fontsize=14, verticalalignment='top',horizontalalignment='left')
 
-plt.figure(figsize=(15, 5))
+plt.figure(figsize=(15, 10))
 plt.subplot(131)
 plot_edges(edges1, threshold1, '前10%的稠密子图')
 plt.subplot(132)
 plot_edges(edges2, threshold2, '前5%的稠密子图')
 plt.subplot(133)
 plot_edges(edges3, threshold3, '前1%的稠密子图')
-plt.show()
+# plt.show()
+plt.savefig('E:\华科实验室论文\MyPythonCode\show_reorder_distribution\graph.png')
